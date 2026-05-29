7023e231426ee8d02b25519ac48e147b367fcaa7
jeffDuhm <jeffduhmzzz@gmail.com>
2026-05-29 14:48:05 -0500

    feat: add logic to render item list

--- renderer/html_renderer.py
+++ renderer/html_renderer.py
@@ -6,10 +6,19 @@ def renderer_html(blocks):
     for block in blocks:
         
         if block["type"] == "heading":
-            html += f"<h{block["level"]}>{block["text"]}</h{block["level"]}>"
+            html += f'<h{block["level"]}>{block["text"]}</h{block["level"]}>'
         
         elif block["type"] == "paragraph":
-            html += f"<p>{block["text"]}</p>"
+            html += f'<p>{block["text"]}</p>'
+        
+        elif block["type"] == "list":
+            
+            list_items = ""
+            
+            for item in block["items"]:
+                list_items += f"<li>{item}</li>"
+                
+            html += f"<ul>{list_items}</ul>"
             
     return html
             
