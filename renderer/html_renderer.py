64b96c8148c8bcb84ccc6221974d62670eb6cf41
jeffDuhm <jeffduhmzzz@gmail.com>
2026-06-13 20:34:20 -0500

    feat: add section opening and closing logic in html renderer

--- renderer/html_renderer.py
+++ renderer/html_renderer.py
@@ -1,9 +1,16 @@
 def renderer_html(blocks, rules):
     
     html = ""
+    open_section = False
     
     for block in blocks:
         
+        if block["type"] in ["heading", "paragraph", "list"]:
+
+            if not open_section:
+                html += "<p>[open-section]</p>"
+                open_section = True
+
         if block["type"] == "heading":
             html += f'<h{block["level"]}>{block["text"]}</h{block["level"]}>'
         
@@ -17,14 +24,19 @@ def renderer_html(blocks, rules):
             for item in block["items"]:
                 list_items += f"<li>{item}</li>"
                 
-            html += f"<ul>{list_items}</ul>"
+            html += f"<ul>{list_items}</ul>" 
         
         elif block["type"] == "widget":
             
+            if open_section:
+                html+=f'<p>[close-section]</p>'
+                open_section = False
+            
             for widget in rules["widgets"]:
-                
                 if widget["id"] == block["widget_id"]:
-                    
                     html += f'<p>{widget["output"]}</p>'
+                    break
+    if open_section:
+        html += f'<p>[close-section]</p>'
             
     return html
